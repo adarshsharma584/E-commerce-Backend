5ef108a9e7d0fa6f26319d83a720e80f31819c32
adarshsharma584 <adarshsharma0862>
2025-07-30 19:10:12 +0530

    added the rest user controllers

--- controllers/user.controller.js
+++ controllers/user.controller.js
@@ -70,58 +70,155 @@ const registerUser = async(req,res)=>{
 
 const loginUser = async(req,res)=>{
     try {
+        const {email,password} = req.body;
+        if(!email || !password){
+            res.status(400).json({message:"All fields are required"});
+        }
+        const existedUser = await User.findOne({email});
+        if(!existedUser){
+            res.status(404).json({message:"User not found"});
+        }
+        const isPasswordMatched = await existedUser.comparePassword(password);
+        if(!isPasswordMatched){
+            res.status(401).json({message:"Invalid credentials"});
+        }
+        const {refreshToken,accessToken} = generateRefreshAndAccessTokens(existedUser);
+        res.cookie("refreshToken",refreshToken,{
+            httpOnly:true,
+            secure:true,
+            sameSite:"none"
+        });
+        existedUser.refreshToken = refreshToken;
+        await existedUser.save();
+
+        res.status(200).json({
+            message:"User logged in successfully",
+            user:{id:existedUser._id,fullName:existedUser.fullName,email:existedUser.email,
+            role:existedUser.role,phoneNo:existedUser.phoneNo},accessToken});
         
     } catch (error) {
-        
+        console.error("Login error:",error);
+        res.status(500).json({message:"Internal server error"});
     }
 }
 
 const logoutUser = async(req,res)=>{
     try {
-        
+        const user = req.user;
+        console.log(user);
+        if(!user){
+            return res.status(401).json({message:"Unauthorized user"});
+        }
+        res.clearCookie("refreshToken");
+        user.refreshToken = "";
+        await user.save();
+        res.status(200).json({message:"User logged out successfully"});
     } catch (error) {
-        
+        console.error("Logout error:",error);
+        res.status(500).json({message:"Internal server error"});
     }
 }
 
 const getUserProfile = async(req,res)=>{
     try {
-        
+        const user =req.user;
+        if(!user){
+            return res.status(401).json({message:"Unauthorized user"});
+        }
+
+        res.status(200).json({message:"User profile fetched successfully",user});
     } catch (error) {
-        
+        console.error("Get user profile error:",error);
+        res.status(500).json({message:"Internal server error"});
     }
 }
 
 const updateUserProfile = async(req,res)=>{
     try {
-        
+        const user = req.user;
+        if(!user){
+            return res.status(401).json({message:"Unauthorized user"});
+        }
+        const {fullName,email,phoneNo,profilePicture} = req.body;
+        console.log(req.body);
+        if(!fullName || !email || !phoneNo || !profilePicture){
+            return res.status(400).json({message:"All fields are required"});
+        }
+         await User.findByIdAndUpdate(user._id,{fullName,email,phoneNo,profilePicture},{new:true});
+
+        const newUpdatedUser = User.findById(user._id);
+
+        if(!newUpdatedUser){
+            return res.status(404).json({message:"User not found"});
+        }
+        res.status(200).json({message:"User profile updated successfully",newUpdatedUser});
+
     } catch (error) {
-        
+        console.error("Update user profile error:",error);
+        res.status(500).json({message:"Internal server error"});
     }
 }
 
 const updateUserPassword = async(req,res)=>{
     try {
-        
+        const user = req.user;
+        if(!user){
+            return res.status(401).json({message:"Unauthorized user"});
+        }
+
+        const {password} = req.body;
+
+        if(!password){
+            return res.status(400).json({message:"Password is required"});
+        };
+
+        const updatedUser = await User.findByIdAndUpdate(user._id,{password},{new:true});
+
+        if(!updatedUser){
+            return res.status(404).json({message:"User not found"});
+        };
+
+        res.status(200).json({message:"User password updated successfully",updatedUser});
+
     } catch (error) {
-        
+        console.error("Update user password error:",error);
+        res.status(500).json({message:"Internal server error"});
     }
 }
 
 const updateUserAddress = async(req,res)=>{
     try {
-        
+        const user = req.user;
+        if(!user){
+            return res.status(401).json({message:"Unauthorized user"});
+        }
+        const {flatNo,buildingName,street,city,state,pincode,country} = req.body;
+        if(!flatNo || !buildingName || !street || !city || !state || !pincode || !country){
+            return res.status(400).json({message:"Address is required"});
+        }
+        const updatedUserAddress = await User.findByIdAndUpdate(user._id,{address:{flatNo,buildingName,street,city,state,pincode,country}},{new:true});
+        if(!updatedUserAddress){
+            return res.status(404).json({message:"User not found"});
+        }
+        res.status(200).json({message:"User address updated successfully",updatedUserAddress});
     } catch (error) {
-        
+        console.error("Update user address error:",error);
+        res.status(500).json({message:"Internal server error"});
     }
 }
 
 const deleteUser = async(req,res)=>{
     try {
-        
+        const user = req.user;
+        if(!user){
+            return res.status(401).json({message:"Unauthorized user"});
+        }
+        await user.remove();
+        res.status(200).json({message:"User deleted successfully"});
     } catch (error) {
-        
+        console.error("Delete user error:",error);
+        res.status(500).json({message:"Internal server error"});
     }
 }
 
-export { registerUser };
+export { registerUser,loginUser,logoutUser,getUserProfile,updateUserProfile,updateUserPassword,updateUserAddress,deleteUser };
---
+++ middlewares/verifyJWT.js
@@ -0,0 +1,16 @@
+import jwt from"jsonwebtoken"
+import User from "../models/user.model.js";
+
+const verifyJWT = (req,res,next)=>{
+    const token = req.cookies?.refreshToken || req.headers?.authorization?.replace("Bearer"," ");
+    if(!token){
+        return res.status(401).json({message:"Unauthorized token"});
+    }
+    jwt.verify(token,process.env.REFRESH_TOKEN_SECRET,(err,user)=>{
+        if(err){
+            return res.status(401).json({message:"Unauthorized"});
+        }
+        req.user = user;
+        next();
+    })
+}
